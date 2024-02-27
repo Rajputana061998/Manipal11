8b015a71d812a0129c56d82dd8e243c67279cb1d
Abhijit Deshmukh <deshabhijit07@gmail.com>
2024-02-27 21:49:40 +0530

    first commit

---
+++ src/main/java/Axis/AXIS1/App.java
@@ -0,0 +1,13 @@
+package Axis.AXIS1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
